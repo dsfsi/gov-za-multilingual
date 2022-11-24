40aaa9f2dd1faf4ea0b67b27270bd5047506a122
lastrucci01 <u20430168@tuks.co.za>
2022-11-24 14:53:50 +0200

    :sparkles: Write to csv's implemented

--- src/govspeech_scrape/main.py
+++ src/govspeech_scrape/main.py
@@ -1,13 +1,15 @@
-import helper_functions 
+import helper_functions
 
 if __name__ == "__main__":
     data = helper_functions.read_JSON_file() # read file 
+    date = data[len(data)-1]["date"] if data != [] else "14 Mar 2013" # fetch latest or default date
+    cabinent_statements = helper_functions.get_cabinent_statements_urls(date) #read cabinet statements until date
 
-    date = data[len(data)-1]["date"] if data != [] else "27 Jan 2006" # fetch latest or default date
-    cabinent_statements = helper_functions.get_cabinent_statements_urls(date) #read cabinet statements until
-
+    new_data = []
     for statement in reversed(cabinent_statements): #reverse the order to store in descending order
+        print('----------------------------')
         extracted = helper_functions.extract_translations(statement['url']) #try extract for each url
-        if extracted: data.append(extracted) #if successful append to current data
+        if extracted: new_data.append(extracted) #if successful append to current data
 
-    helper_functions.write_JSON_file(data) # write file
+    helper_functions.update_all_csv(new_data)
+    helper_functions.write_JSON_file(data+new_data) # write file (data+new_data will concat the lists)
