f1bcc6dadee694d42bf2a843ebe04e47c9738ed0
lastrucci01 <u20430168@tuks.co.za>
2023-05-31 14:33:55 +0200

    can't make a dataframe from a dict unless it's wrapped in a list ... terrible

--- src/sentence_alignment/main.py
+++ src/sentence_alignment/main.py
@@ -39,7 +39,7 @@ def align_files(source_file, target_file, source_lang, target_lang, f, g):
         used_sentences.append(max_similar)
 
         # Create dataframe to store sentences
-        temp = pd.DataFrame.from_dict({'src_text': f['text'][i], 'trg_text': g['text'][max_similar], 'cosine_score': similarity_array[max_similar]})
+        temp = pd.DataFrame.from_dict([{'src_text': f['text'][i], 'trg_text': g['text'][max_similar], 'cosine_score': similarity_array[max_similar]}])
         df = pd.concat([df, temp], ignore_index=True)
 
     # Output full dataframe to a csv file
