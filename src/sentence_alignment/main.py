e9595d8c8871ab7490d0873de9aa10a636dd1c3d
idzingirai <ishe.dzingirai@gmail.com>
2025-06-13 06:33:42 +0200

    Some language text such as nso are missing from the scrapped data so checks were added before attempting to align content

--- src/sentence_alignment/main.py
+++ src/sentence_alignment/main.py
@@ -42,25 +42,25 @@ if __name__ == "__main__":
     c.download_laser_models(lang_model_map)
     c.download_tokeniser()
 
-
     for statement in cab_statements:
         if statement["datetime"] > lastdate:
             print(statement["datetime"] + " // " + lastdate)
             for k in lang_map.keys():
-                tokens = sa.tokenise(lang_map[k], statement[k]["text"])
-                date = statement["datetime"]
-                processed = sa.pre_process_text(lang_map[k], statement[k]["text"])
-                f.write_raw_to_file(date, lang_map[k], statement[k]["text"])
-                f.write_raw_to_file('{}_processed'.format(date), lang_map[k], processed)
-                f.write_tokens_to_file(date, lang_map[k], tokens)
-                se.encode_sentence_tokens(date, lang_map[k], lang_model_map[lang_map[k]])
+                if k in statement:
+                    tokens = sa.tokenise(lang_map[k], statement[k]["text"])
+                    date = statement["datetime"]
+                    processed = sa.pre_process_text(lang_map[k], statement[k]["text"])
+                    f.write_raw_to_file(date, lang_map[k], statement[k]["text"])
+                    f.write_raw_to_file('{}_processed'.format(date), lang_map[k], processed)
+                    f.write_tokens_to_file(date, lang_map[k], tokens)
+                    se.encode_sentence_tokens(date, lang_map[k], lang_model_map[lang_map[k]])
 
     langs = lang_model_map.keys()
     lang_pairs = list(combinations(langs, 2))
     new_lastdate = lastdate
     for statement in cab_statements:
         for (src_lang, tgt_lang) in lang_pairs:
-            if statement["datetime"] > lastdate:
+            if statement["datetime"] > lastdate and (src_lang in statement and tgt_lang in statement):
                 sa.sentence_alignment(src_lang, tgt_lang, statement["datetime"])
 
         if statement["datetime"] > lastdate:
