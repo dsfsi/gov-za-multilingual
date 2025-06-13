f2d0b5d40d66f5ed778abb998441955b9e09caa2
idzingirai <ishe.dzingirai@gmail.com>
2025-06-13 08:07:43 +0200

    Debugging laser path

--- src/sentence_alignment/config.py
+++ src/sentence_alignment/config.py
@@ -1,7 +1,7 @@
 import os, subprocess, nltk
-from pathlib import Path
 
-LASER_PATH = os.path.join(Path().resolve(), 'LASER') # path to LASER module
+LASER_PATH = os.path.join(os.getcwd(), 'LASER')  # path to LASER module
+
 
 def set_environ_var():
     """
@@ -9,23 +9,26 @@ def set_environ_var():
     """
     print(f'Laser Path: {LASER_PATH}')
     os.environ['LASER'] = str(LASER_PATH)
-    os.environ['LC_ALL']='C.UTF-8'
-    os.environ['LANG']='C.UTF-8'
+    os.environ['LC_ALL'] = 'C.UTF-8'
+    os.environ['LANG'] = 'C.UTF-8'
+
 
 def setup_laser():
     """
     ### Downloads configs for LASER repo
     """
     print('Setting Up LASER module...')
-    if os.path.exists(os.path.join(LASER_PATH,'models')):
-        print('LASER/models dir found, skipping installation, delete folders for and run script again for fresh installation')
-    else: 
+    if os.path.exists(os.path.join(LASER_PATH, 'models')):
+        print(
+            'LASER/models dir found, skipping installation, delete folders for and run script again for fresh installation')
+    else:
         command = f'bash {os.path.join(LASER_PATH, 'install_models.sh')}'
         subprocess.run(command, shell=True)
-        
+
     if os.path.exists(f'{os.path.join(LASER_PATH, 'tools-external')}'):
-        print('LASER/tools-external dir found, skipping installation, delete folders for and run script again for fresh installation')
-    else: 
+        print(
+            'LASER/tools-external dir found, skipping installation, delete folders for and run script again for fresh installation')
+    else:
         command = f'bash {os.path.join(LASER_PATH, 'install_external_tools.sh')}'
         subprocess.run(command, shell=True)
     print('LASER module configured.')
@@ -39,7 +42,7 @@ def download_laser_models(lang_mappings):
     """
     print('Downloading LASER models...')
     command = f'bash {LASER_PATH}/nllb/download_models.sh'
-    for _,val in lang_mappings.items():
+    for _, val in lang_mappings.items():
         command = "{} {}".format(command, val)
     subprocess.run(command, shell=True)
     print('Models downloaded.')
